a088df0ec0924a6da52e0c878266e9f2b29a8b38
Larkin Crain <lcrain@icumedicine.com>
2015-02-11 10:50:18 -0600

    Added Main Module
    Created a file called Main.cpp where all other code will be accessed
    from.

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+/*
+Main Module
+
+This module is where out program will start and where all other modules and functions
+are called. It will contain the main loop and will link to the other modules.
+ */
+
+//Main Loop
+void loop() {
+
+	//GLOBALS -- Keep the number of these to a minimum
+	
+	//END GLOBALS
+	
+	
+	
+	
+
+
+}
