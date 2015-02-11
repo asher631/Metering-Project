fed319db5e511b33c310a8617c79d1fff0ced648
Larkin Crain <lcrain@icumedicine.com>
2015-02-11 11:10:46 -0600

    Included libraries
    Added libraries for communicating with our metering circuit, our screen,
    and a low power sleep library to test.

--- Main.cpp
+++ Main.cpp
@@ -5,6 +5,12 @@ This module is where out program will start and where all other modules and func
 are called. It will contain the main loop and will link to the other modules.
  */
 
+ //Libraries to include
+ #include <Input/Metering/ADE7753.h>							//ADE7753 communication library
+ #include <Output/Screen/Adafruit-GFX-library/Adafruit_GFX.h>	//Library that the sharp memory library extends
+ #include <Output/Screen/SharpMemoryLCD/SharpMemoryLcd.h>		//Sharp Memory LCD communication library
+ #include <PowerSaving/RocketScreamLibrary/LowPower/h>			//Provides powerful low-power sleep functions
+ 
 //Main Loop
 void loop() {
 
