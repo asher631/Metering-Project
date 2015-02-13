b1405e9dab8922c1ef07a6a04b056a22fed271b3
Larkin Crain <larkincrain@gmail.com>
2015-02-12 19:51:08 -0600

    Worked on the Screen class.
    Added basic methods of the screen class to ensure that we have a
    skeleton to work off of.

--- Output/Screen/Screen.h
+++ Output/Screen/Screen.h
@@ -1,3 +1,29 @@
 /*
-	This module will define how the arduino will communicate with the screen that we will integrate with out project.
+* Title: Screen.h
+* Authors: Larkin Crain
+* Group: The A Team
+* For: Senior Design - Metering Project
+* Date: 2/12/2015
+*
+* Library definition for interfacing with the screen that we will use. Currently, this is the 
+* Sharp Memory LCD 2.7in.
+*
 */
+
+//Prevents issues with accidental double linking
+#ifndef Screen_h
+#define Screen_h
+
+#include "Adafruit-GFX-library/Adafruit_GFX.h"
+#include "SharpMemoryLCD/SharpMemopryLcd.h"
+
+class Screen {
+	
+	public:
+		bool Write(char message[]);		//Send a message to the screen
+		bool Clear();					//Clear the screen
+		bool Flash();					//Quickly make the screen flash (by inverting and un-inverting it quickly)
+	private:
+	
+	
+}
