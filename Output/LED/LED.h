86a500b0b8150e23cdb50c57ca84ec9f7a0f089f
asher631 <asher631@hotmail.com>
2015-02-18 17:40:17 -0600

    Update LED.h

--- Output/LED/LED.h
+++ Output/LED/LED.h
@@ -1,3 +1,24 @@
-/*
-	This module will define how the arduino will communicate with the variety of LEDs that the project will integrate.
-*/
+Title: LED.h
++* Authors: Clark Asher
++* Group: The A Team
++* For: Senior Design - Metering Project
++* Date: 2/18/2015
++*
++* Library definition for controlling LEDs
++*
++*/
++
++
++#ifndef LED_h
++#define LED_h
++
++class Bargraph
++{
++	public:
++
++		Bargraph();
++	
++		unsigned int ConvertPower();   //converts instantaneous power to binary data
++		bool LEDwrite();   //triggers LEDs for bargraph
++
++};
